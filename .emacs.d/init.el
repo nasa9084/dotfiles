352a0945b40ff1c85f18c23514b4d41777d2fcda
nasa9084 <nasa9084@live.jp>
2016-05-24 10:58:00 +0900

    add some packages

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -102,13 +102,16 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(company-idle-delay nil)
+ '(custom-safe-themes
+   (quote
+    ("33e6cfb654f07e565038fbb7ae00592dde82d72f75d6cb76bbe7052cd45fcce4" default)))
  '(flycheck-display-errors-function (function flycheck-pos-tip-error-messages))
  '(mode-line-format
    (quote
     ("" mode-line-mule-info mode-line-modified "-" mode-line-buffer-identification "%p / L%l:C%c (" mode-name mode-line-process minor-mode-alist ")")))
  '(package-selected-packages
    (quote
-    (which-key cyphejor company-quickhelp org web-mode yasnippet company state htmlize ox-reveal github-browse-file visual-regexp geeknote poporg flycheck-pos-tip magit markdown-mode helm-config helm mode-compile smart-compile codic hiwin yatex volatile-highlights use-package undohist undo-tree twittering-mode stash smooth-scroll smart-newline multi-term haskell-mode flycheck emmet-mode bury-successful-compilation auto-install auto-compile auto-async-byte-compile)))
+    (dockerfile-mode coffee-mode js2-mode which-key cyphejor company-quickhelp org web-mode yasnippet company state htmlize ox-reveal github-browse-file visual-regexp geeknote poporg flycheck-pos-tip magit markdown-mode helm-config helm mode-compile smart-compile codic hiwin yatex volatile-highlights use-package undohist undo-tree twittering-mode stash smooth-scroll smart-newline multi-term haskell-mode flycheck emmet-mode bury-successful-compilation auto-install auto-compile auto-async-byte-compile)))
  '(tab-width 4))
 
 ;; yes or no -> y or n
@@ -478,6 +481,9 @@
   :init
   ;; 文法チェック
   (add-hook 'after-init-hook #'global-flycheck-mode)
+
+  ;; add list
+  (flycheck-add-mode 'javascript-eslint 'js2-jsx-mode)
 
   ;; エラーをツールチップ表示
   (eval-after-load 'flycheck
