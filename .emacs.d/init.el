0f8dd2d6039f0792113e925a0c2720ec307fb206
nasa9084 <nasa9084@live.jp>
2016-10-15 22:20:19 +0900

    fix: melpa path

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -30,7 +30,7 @@
 
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives
              '("marmalade" . "https://marmalade-repo.org/packages/") t)
 
