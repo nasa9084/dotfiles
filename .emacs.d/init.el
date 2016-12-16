5adf67eb467c88986d6ba96cbd20084ef282094c
nasa9084 <nasa9084@live.jp>
2016-12-17 04:12:40 +0900

    remove: unnecessary

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -933,12 +933,7 @@
 
 ;; テーマ選択
 (load-theme 'gnupack-dark t)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
 ;; Local Variables:
 ;; coding: utf-8
 ;; mode: emacs-lisp
