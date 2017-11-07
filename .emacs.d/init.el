69f99e8b093d6c42483f8998169826012c1e7435
nasa9084 <nasa9084@live.jp>
2017-11-07 13:27:48 +0900

    fix: gofmt-before-save

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -533,7 +533,8 @@
   (setq gofmt-command "goimports")
   (add-hook 'go-mode-hook (lambda()
                             (local-set-key (kbd "M-.") 'godef-jump)))
-  (add-hook 'before-save-hook 'gofmt-before-save nil t))
+  (add-hook 'before-save-hook 'gofmt-before-save))
+
 
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
 ;;; @ helm
