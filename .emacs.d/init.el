6f6e04ebb3d2a18fda9e7f3a7640c045c16457da
nasa9084 <nasa9084@live.jp>
2018-01-18 11:44:41 +0900

    unuse undohist

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -878,21 +878,6 @@
       (setq-local browse-url-browser-function 'browse-url-default-browser))))
 
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
-;;; @ undohist
-;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
-
-;; undoの永続化
-(use-package undohist
-  :ensure t
-  :config
-  (undohist-initialize)
-  ;; 永続化保存ファイルの場所
-  (setq undohist-directory "/var/tmp/undohist")
-  ;; 永続化を無視するファイル名の正規表現
-  (setq undohist-ignored-files
-        '("/tmp/")))
-
-;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
 ;;; @ undo-tree
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
 
