c4e379a1aeeea62c8f3da6d69f9fa63d70c402bf
nasa9084 <nasa.9084.bassclarinet@gmail.com>
2020-01-10 18:38:27 +0900

    remove marmalade from package archive list

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -31,8 +31,6 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives
-             '("marmalade" . "https://marmalade-repo.org/packages/") t)
 
 (package-initialize)
 
