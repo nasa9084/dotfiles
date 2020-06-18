7162c36c5f2d0d849cb85e59bab7fd69fcf194c8
nasa9084 <nasa.9084.bassclarinet@gmail.com>
2020-06-18 13:42:00 +0900

    add Hack Nerd Font

--- .hyper.js
+++ .hyper.js
@@ -12,7 +12,7 @@ module.exports = {
         fontSize: 14,
 
         // font family with optional fallbacks
-        fontFamily: 'Ricty Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+        fontFamily: 'Ricty, "Hack Nerd Font", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
         // default font weight: 'normal' or 'bold'
         fontWeight: 'normal',
