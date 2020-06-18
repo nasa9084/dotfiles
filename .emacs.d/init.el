2fb13880133ad074629118d150b3e71f78665296
nasa9084 <nasa.9084.bassclarinet@gmail.com>
2020-06-18 18:26:04 +0900

    enable display-line-numbers -mode to text-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -73,6 +73,7 @@
       (format "%%f - Emacs@%s" (system-name)))
 
 ;; 行番号表示
+(add-hook 'text-mode-hook #'display-line-numbers-mode)
 (add-hook 'prog-mode-hook #'display-line-numbers-mode)
 
 ;; フォント設定
