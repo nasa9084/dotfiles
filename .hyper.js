5853c19b4d81207459b3a97d8517e6ab7b223812
nasa9084 <nasa.9084.bassclarinet@gmail.com>
2021-07-02 00:00:44 +0900

    install hyper-quit plugin

--- .hyper.js
+++ .hyper.js
@@ -131,7 +131,10 @@ module.exports = {
     //   `hyperpower`
     //   `@company/project`
     //   `project#1.0.1`
-    plugins: ["hyper-oceanic-next"],
+    plugins: [
+        "hyper-oceanic-next",
+        "hyper-quit",
+    ],
 
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
